bb9ff7c34cdd54ba458d7872167e35cb3757f5da
rthete <reikattt@qq.com>
2023-12-22 14:57:03 +0800

    formatting

--- Processor.py
+++ Processor.py
@@ -23,13 +23,27 @@ logging.basicConfig(
 
 
 class Processor:
+    """The whole crack detection and measurement process."""
+    
     @staticmethod
     def segment_UNet(original_img):
-        """Stage 1. Use pretrained UNet model to predict crack areas."""
+        """Use pretrained UNet model to predict crack areas.
+
+        Args:
+            original_img (Image): The original image.
+
+        Returns:
+            segmented_img (nparray): The segmented image.
+            positive_flag (bool): Flag is true when image has crack area.
+        """
 
         count = False
-        resnet50_unet = Model(model_path='pth/resnet50_unet_best_epoch_weights.pth', backbone="resnet50")
-        vgg_unet = Model(model_path='pth/vgg_unet_best_epoch_weights.pth', backbone="vgg")
+        resnet50_unet = Model(
+            model_path="pth/resnet50_unet_best_epoch_weights.pth", backbone="resnet50"
+        )
+        vgg_unet = Model(
+            model_path="pth/vgg_unet_best_epoch_weights.pth", backbone="vgg"
+        )
         name_classes = ["background", "crack"]
 
         pr_resnet50 = resnet50_unet.get_pr(
@@ -42,7 +56,7 @@ class Processor:
             pr_resnet50_flat, return_counts=True
         )
         num_classes_resnet50 = len(unique_classes_resnet50)
-        
+
         pr_vgg_flat = pr_vgg.flatten()
         unique_classes_vgg, class_counts_vgg = np.unique(
             pr_vgg_flat, return_counts=True
@@ -98,8 +112,15 @@ class Processor:
 
     @staticmethod
     def edge_detection(original_img, segmented_img):
-        """Stage 2. Edge detection to further locate crack areas."""
+        """Edge detection to further locate crack areas.
 
+        Args:
+            original_img (Image): The original image.
+            segmented_img (nparray): The segmented regions obtained through the segmentation model.
+
+        Returns:
+            result_img (nparray): The more precise crack regions obtained by combining edge detection.
+        """
         original_img = np.array(original_img)
         try:
             original_img = cv2.cvtColor(original_img, cv2.COLOR_RGB2GRAY)
@@ -122,9 +143,19 @@ class Processor:
 
     @staticmethod
     def measure_incircle(index, original_img, edge_detected_img):
-        """Stage 3. Incircle algorithm to measure segmented crack areas' width."""
-        
-        original_img = np.array(original_img)
+        """Incircle algorithm to measure segmented crack areas' width.
+
+        Args:
+            index (int): The block's index.
+            original_img (Image): The original image.
+            edge_detected_img (nparray): The crack regions.
+
+        Returns:
+            result_img (nparray): The image visualizing the results of width measurements.
+
+        """
+
+        result_img = np.array(original_img)
         contours_arr, _ = cv2.findContours(
             edge_detected_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE
         )
@@ -140,10 +171,10 @@ class Processor:
 
             label.append(max_val * 2)
             radius = int(max_val)
-            
-            cv2.circle(original_img, max_dist_pt, radius, (0, 0, 255), 1, 1, 0)
+
+            cv2.circle(result_img, max_dist_pt, radius, (0, 0, 255), 1, 1, 0)
             cv2.putText(
-                original_img,
+                result_img,
                 f"{label[-1]:.2f}",
                 (max_dist_pt[0] + radius + 5, max_dist_pt[1]),
                 cv2.FONT_HERSHEY_SIMPLEX,
@@ -152,34 +183,47 @@ class Processor:
                 1,
                 cv2.LINE_AA,
             )
-        
+
         logging.info(f"Widths in block {index}: {label}")
 
-        return original_img
+        return result_img
 
     @staticmethod
     def add_mask(segmented_img_array, old_img_array):
-        """Stage 3. Add mask for result visualization."""
+        """Add mask for result visualization.
         
+        Args:
+            segmented_img_array  (nparray): The crack regions.
+            old_img_array (nparray): The original image. 
+            
+        Returns:
+            result_array (nparray): The original block with additional added shadows.
+        """
+
         segmented_img_Image = Image.fromarray(segmented_img_array)
         mask = np.all(segmented_img_Image == [128, 0, 0], axis=-1)
 
         alpha = 0.7
-        mixed_pixels = (1 - alpha) * old_img_array[mask] + alpha * segmented_img_array[mask]
+        mixed_pixels = (1 - alpha) * old_img_array[mask] + alpha * segmented_img_array[
+            mask
+        ]
         old_img_array[mask] = mixed_pixels
         old_img_Image = Image.fromarray(old_img_array)
-        
+
         blend_factor = 0.5
         result_Image = Image.blend(old_img_Image, segmented_img_Image, blend_factor)
-        
-        return np.array(result_Image)
+        result_array = np.array(result_Image)
+
+        return result_array
 
     @staticmethod
     def split_image(img_path, block_size):
         image = Image.open(img_path)
         img_width, img_height = image.size
         blocks = []
-        logging.info(f"Splitting original image...original image size: `({str(img_height)}, {str(img_width)})`")
+        logging.info(
+            f"Splitting original image...original image size: `({str(img_height)}, {str(img_width)})`"
+        )
         for y in range(0, img_height, block_size):
             for x in range(0, img_width, block_size):
                 box = (x, y, x + block_size, y + block_size)
--- predict.py
+++ predict.py
@@ -9,12 +9,15 @@ from Processor import Processor
 
 Image.MAX_IMAGE_PIXELS = None
 
+
 def process_pipeline(index, original_block):
     segmented_block, positive_flag = Processor.segment_UNet(original_block)
     if not positive_flag:
         return np.array(original_block)
     edge_detected_block = Processor.edge_detection(original_block, segmented_block)
-    measured_block = Processor.measure_incircle(index, original_block, edge_detected_block)
+    measured_block = Processor.measure_incircle(
+        index, original_block, edge_detected_block
+    )
     result_block = Processor.add_mask(measured_block, np.array(original_block))
     return result_block
     # return measured_block
@@ -26,7 +29,7 @@ if __name__ == "__main__":
     for root, dirs, files in os.walk(sample_path):
         for file in files:
             image_path_list.append(os.path.join(root, file))
-    
+
     for image_path in image_path_list:
         original_image = Image.open(image_path)
         blocks = Processor.split_image(image_path, 1024)
@@ -35,6 +38,10 @@ if __name__ == "__main__":
         for block in blocks:
             index = index + 1
             processed_blocks.append(process_pipeline(index, block))
-            
-        processed_image = Processor.join_image(processed_blocks, original_image.width, original_image.height)
-        processed_image.save('output/predict_result_{}'.format(os.path.basename(image_path)))
+
+        processed_image = Processor.join_image(
+            processed_blocks, original_image.width, original_image.height
+        )
+        processed_image.save(
+            "output/predict_result_{}".format(os.path.basename(image_path))
+        )
